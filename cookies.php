b19eaf98b7223476b82cf6571a21baf0b1e0258f
Jay Versluis <versluis2000@yahoo.com>
2026-06-22 22:15:07 -0400

    modernised and security patched

--- cookies.php
+++ cookies.php
@@ -1,212 +1,193 @@
 <?php
-
 /**
  * Plugin Name: Show Me The Cookies
- * Plugin URI: https://wpguru.co.uk/2019/03/show-me-the-cookies-how-to-list-all-cookies-on-your-wordpress-site/
- * Description: Displays a list of all cookies used on your site
- * Version: 1.0
- * Author: Jay Versluis
- * Author URI: https://wpguru.tv
- * License: GPL2
- * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin URI:  https://wpguru.co.uk/2019/03/show-me-the-cookies-how-to-list-all-cookies-on-your-wordpress-site/
+ * Description: Display a list of all cookies used on your site, or view them under Appearance - Cookies.
+ * Version:     1.1
+ * Author:      Jay Versluis
+ * Author URI:  https://wpguru.tv
+ * License:     GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: show-me-the-cookies
  */
- 
-/*  Copyright 2019  Jay Versluis (email support@wpguru.tv)
+
+/*  Copyright 2019 Jay Versluis (email: support@wpguru.tv)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-// add submenu page under Appearance
-// https://codex.wordpress.org/Function_Reference/add_theme_page
-function smtc_cookies_menu() {
-	// add_theme_page('My Plugin Theme', 'My Plugin', 'edit_theme_options', 'my-unique-identifier', 'my_plugin_function');
-	add_theme_page('Show Me The Cookies', 'Cookies', 'edit_theme_options', 'gcookies', 'smtc_guru_cookies');
+// Prevent direct file access.
+defined( 'ABSPATH' ) || exit;
+
+class Show_Me_The_Cookies {
+
+	private static bool $frontend_styles_done = false;
+
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'register_menu' ] );
+		add_shortcode( 'cookies',      [ $this, 'shortcode_all_cookies' ] );
+		add_shortcode( 'cookies-nowp', [ $this, 'shortcode_nowp_cookies' ] );
+	}
+
+	// Register the admin menu item under Appearance.
+	// https://developer.wordpress.org/reference/functions/add_theme_page/
+	public function register_menu(): void {
+		add_theme_page(
+			__( 'Show Me The Cookies', 'show-me-the-cookies' ),
+			__( 'Cookies', 'show-me-the-cookies' ),
+			'manage_options',
+			'gcookies',
+			[ $this, 'render_admin_page' ]
+		);
+	}
+
+	public function render_admin_page(): void {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( esc_html__( 'You do not have sufficient privileges to access this page.', 'show-me-the-cookies' ) );
+		}
+		?>
+		<style>
+			.smtc-cookie-list { list-style: none; padding-left: 0; }
+			.smtc-cookie-list li { margin-bottom: 1em; }
+		</style>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Show Me The Cookies', 'show-me-the-cookies' ); ?></h1>
+			<p><?php esc_html_e( "Here's a list of all current cookies used on your site.", 'show-me-the-cookies' ); ?></p>
+			<hr>
+			<?php echo wp_kses_post( $this->build_cookie_list() ); ?>
+			<hr>
+			<p>
+				<?php esc_html_e( 'To display this list to your visitors, use the shortcode', 'show-me-the-cookies' ); ?>
+				<strong>[cookies]</strong>.
+			</p>
+			<p>
+				<?php esc_html_e( 'You can filter out all WordPress related cookies using', 'show-me-the-cookies' ); ?>
+				<strong>[cookies-nowp]</strong>.
+			</p>
+			<p>
+				<?php esc_html_e( 'Find out more about cookies on', 'show-me-the-cookies' ); ?>
+				<a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">Wikipedia</a>.
+			</p>
+			<hr>
+			<p>
+				<a href="https://wpguru.co.uk" target="_blank">
+					<img src="<?php echo esc_url( plugins_url( 'images/guru-header-2013.png', __FILE__ ) ); ?>" width="300" alt="WP Guru">
+				</a>
+			</p>
+			<p>
+				<a href="https://wpguru.co.uk/2019/03/show-me-the-cookies-how-to-list-all-cookies-on-your-wordpress-site/" target="_blank">Plugin by Jay Versluis</a>
+				| <a href="https://github.com/versluis/Cookies" target="_blank">Contribute on GitHub</a>
+				| <a href="https://patreon.com/versluis" target="_blank">Support me on Patreon</a>
+			</p>
+		</div>
+		<?php
+	}
+
+	// Shortcode: [cookies] — lists all cookies.
+	// https://developer.wordpress.org/reference/functions/add_shortcode/
+	public function shortcode_all_cookies( $atts = [], string $content = '' ): string {
+		$atts       = is_array( $atts ) ? $atts : [];
+		$show_value = $this->should_show_value( $atts );
+
+		$cookies = $_COOKIE;
+		ksort( $cookies );
+
+		$count  = 1;
+		$output = $this->maybe_frontend_styles();
+		$output .= '<ol class="smtc-cookie-list">';
+		foreach ( $cookies as $key => $val ) {
+			$output .= '<li>';
+			$output .= '<strong>Cookie #' . $count . '</strong>: ' . esc_html( $key );
+			if ( $show_value ) {
+				$output .= '<br>Value: ' . esc_html( $val );
+			}
+			$output .= '</li>';
+			$count++;
+		}
+		$output .= '</ol>';
+
+		return $output;
+	}
+
+	// Shortcode: [cookies-nowp] — same as above but skips WordPress core cookies.
+	public function shortcode_nowp_cookies( $atts = [], string $content = '' ): string {
+		$atts       = is_array( $atts ) ? $atts : [];
+		$show_value = $this->should_show_value( $atts );
+
+		$cookies  = $_COOKIE;
+		$filtered = array_filter( $cookies, fn( $key ) => ! $this->is_wp_cookie( $key ), ARRAY_FILTER_USE_KEY );
+		ksort( $filtered );
+
+		if ( empty( $filtered ) ) {
+			return '<p>' . esc_html__( 'No non-WordPress cookies found.', 'show-me-the-cookies' ) . '</p>';
+		}
+
+		$count  = 1;
+		$output = $this->maybe_frontend_styles();
+		$output .= '<ul class="smtc-cookie-list">';
+		foreach ( $filtered as $key => $val ) {
+			$output .= '<li>';
+			$output .= '<strong>Cookie #' . $count . '</strong>: ' . esc_html( $key );
+			if ( $show_value ) {
+				$output .= '<br>Value: ' . esc_html( $val );
+			}
+			$output .= '</li>';
+			$count++;
+		}
+		$output .= '</ul>';
+
+		return $output;
+	}
+
+	// Outputs the frontend CSS once per page, even if both shortcodes are used.
+	private function maybe_frontend_styles(): string {
+		if ( self::$frontend_styles_done ) {
+			return '';
+		}
+		self::$frontend_styles_done = true;
+		return '<style>.smtc-cookie-list{list-style:none;padding-left:0}.smtc-cookie-list li{margin-bottom:1em}</style>';
+	}
+
+	// Builds the cookie list for the admin page. Always shows values; no separator option needed.
+	private function build_cookie_list(): string {
+		$cookies = $_COOKIE;
+		ksort( $cookies );
+
+		$count  = 1;
+		$output = '<ol class="smtc-cookie-list">';
+		foreach ( $cookies as $key => $val ) {
+			$output .= '<li>';
+			$output .= '<strong>Cookie #' . $count . '</strong>: ' . esc_html( $key ) . '<br>';
+			$output .= 'Value: ' . esc_html( $val );
+			$output .= '</li>';
+			$count++;
+		}
+		$output .= '</ol>';
+
+		return $output;
+	}
+
+	// Returns false when the positional 'novalue' shortcode attribute is present.
+	// Usage: [cookies novalue] suppresses the cookie value in the output.
+	private function should_show_value( array $atts ): bool {
+		return ! ( isset( $atts[0] ) && strtolower( $atts[0] ) === 'novalue' );
+	}
+
+	// A cookie name is considered a WordPress cookie if it contains 'wordpress' or 'wp'.
+	private function is_wp_cookie( string $name ): bool {
+		return str_contains( $name, 'wordpress' ) || str_contains( $name, 'wp' );
+	}
 }
-add_action('admin_menu', 'smtc_cookies_menu');
 
-// *********************
-// Main Plugin Function
-// *********************
-function smtc_guru_cookies () {
-	
-	// check that the user has the required capability 
-    if (!current_user_can('manage_options'))
-    {
-      wp_die( __('You do not have sufficient privileges to access this page. Sorry!') );
-    }	
-	
-	///////////////////////////////////////
-	// MAIN AMDIN CONTENT SECTION
-	///////////////////////////////////////
-	
-	// display heading with icon WP style
-	?>
-    <div class="wrap">
-    <div id="icon-index" class="icon32"><br></div>
-    <h2>Show Me The Cookies</h2>
-    <p>Here's a list of all current cookies used on your site.</p>
-    <hr>
-	<?php
-	// call cookies function here
-	echo smtc_guru_admin_cookies();
-	?>
-    <hr>
-    <p>To display this list to your visitors, use the shortcode <strong>[cookies]</strong> in your post or page.</p>
-    <p>You can filter out all WordPress related cookies using the shortcode <strong>[cookies-nowp]</strong>.</p>
-    <p>If you like, you can replace the default separator between the cookie and its value <br>by placing your own string inside the opening and closing tags.<br>For example <strong>[cookies] ==> [/cookies]</strong>.
-    <p>Find out more about Cookies on <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">Wikipedia</a>.</p> 
-    <hr>
-    
-    <?php
-    // ***************
-    // DISPLAY FOOTER 
-	// ***************
-	?>
-	<p><a href="https://wpguru.co.uk" target="_blank"><img src="<?php  
-	echo plugins_url('images/guru-header-2013.png', __FILE__); ?>" width="300"></a> </p>
-
-<p><a href="https://wpguru.co.uk/2019/03/show-me-the-cookies-how-to-list-all-cookies-on-your-wordpress-site/" target="_blank">Plugin by Jay Versluis</a> | <a href="https://github.com/versluis/Cookies" target="_blank">Contribute on GitHub</a> | <a href="https://patreon.com/versluis" target="_blank">Support me on Patreon</a></p>
-
-<p><span><!-- Social Buttons -->
-
-<!-- YouTube -->
-<script src="https://apis.google.com/js/platform.js"></script>
-<div class="g-ytsubscribe" data-channel="wphosting"></div>
-
-<!-- Place this tag after the last widget tag. -->
-<script type="text/javascript">
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
-
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-<!-- Twitter -->
-<a href="https://twitter.com/versluis" class="twitter-follow-button" data-show-count="true">Follow @versluis</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-<!-- Facebook -->
-<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FThe-WP-Guru%2F162188713810370&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=186277158097599" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
-
-</span></p>
-</div>
-<?php
-	
-	}
-	// end of Main Function
-
-// ********************************
-// Additional and Helper Functions
-// ********************************
-function smtc_guru_get_nowp_cookies( $paras = '', $content = '' ) {
-	
-	$novalue = false;
-	if ( !empty( $paras[0] ) ) {	
-	  if ( strtolower( $paras[ 0 ] ) == 'novalue' ) {
-		  $novalue = true;
-	  } 
-	}
-	
-	if ( $content == '' ) {
-		$seperator = ' : ';
-	} else {
-		$seperator = $content;
-	}
-	$cookie = $_COOKIE;
-	ksort( $cookie );
-	$content = "<ul>";
-	foreach ( $cookie as $key => $val ) {
-		
-		// don't do this if key is 'wordpress' or 'wp'
-		if (!smtc_isThisInThat ('wordpress', $key) && !smtc_isThisInThat ('wp', $key)) {
-		$content .= '<li><b>' . $key . '</b>';
-		if ( !$novalue ) {
-			$content .= $seperator . $val; 
-		}
-		$content .= "</li>"; 
-		
-		}
-	} 
-	$content .= "</ul>"; 
-	return do_shortcode( $content ); 
-} 
-// adding the above function to WordPress
-// https://codex.wordpress.org/Function_Reference/add_shortcode
-add_shortcode( 'cookies-nowp', 'smtc_guru_get_nowp_cookies' );
-
-// same as above, but listing all cookies
-function smtc_guru_get_cookies( $paras = '', $content = '' ) {
-	
-	$novalue = false;
-	if ( !empty( $paras[0] ) ) {	
-	  if ( strtolower( $paras[ 0 ] ) == 'novalue' ) {
-		  $novalue = true;
-	  } 
-	}
-	
-	if ( $content == '' ) {
-		$seperator = ' : ';
-	} else {
-		$seperator = $content;
-	}
-	$cookie = $_COOKIE;
-	ksort( $cookie );
-	$content = "<ol>";
-	foreach ( $cookie as $key => $val ) {
-		
-		// list all cookies
-		$content .= '<li><b> ' . $key . '</b>';
-		if ( !$novalue ) {
-			$content .= $seperator . $val; 
-		}
-		$content .= "</li>"; 
-	} 
-	$content .= "</ol>"; 
-	return do_shortcode( $content ); 
-} 
-add_shortcode( 'cookies', 'smtc_guru_get_cookies' );
-
-// helper function to determine if a phrase is in another string
-// https://stackoverflow.com/questions/4366730/how-do-i-check-if-a-string-contains-a-specific-word
-function smtc_isThisInThat ($needle, $haystack) {
-	return strpos($haystack, $needle) !==false;
-}
-
-// list cookies in admin interface
-function smtc_guru_admin_cookies () {
-	
-	if ( $content == '' ) {
-		$seperator = ' : ';
-	} else {
-		$seperator = $content;
-	}
-	
-	$cookie = $_COOKIE;
-	ksort( $cookie );
-	$content = "<ol>";
-	
-	foreach ( $cookie as $key => $val ) {
-		$content .= '<li><b> ' . $key . '</b>';
-		if ( !$novalue ) {
-			$content .= $seperator . $val; 
-		}
-		$content .= "</li>"; 
-	} 
-	$content .= "</ol>";
-	 
-	return $content;
-}
+new Show_Me_The_Cookies();
--- readme.txt
+++ readme.txt
@@ -1,36 +1,36 @@
 === Show Me The Cookies ===
 Contributors: versluis
-Donate link: https:patreon.com/versluis
-Tags: cookie, cookies, gdpr, local storage
-Requires at least: 3.4
-Tested up to: 5.1
-Stable tag: 1.0
-Requires PHP: 5.6.4
+Donate link: https://patreon.com/versluis
+Tags: cookie, cookies, gdpr, privacy
+Requires at least: 5.9
+Tested up to: 7.0
+Stable tag: 1.1
+Requires PHP: 8.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Shows a list of all current cookies on your site, both for admins and visitors (optional). 
+Shows a list of all current cookies on your site, both for admins and visitors (optional).
 
 == Description ==
 
-Show Me The Cookies displays all current cookies your site is using. Once activated, you can find this list under Appearance - Cookies.
+Show Me The Cookies displays all current cookies your site is using. Once activated, you can find this list under **Appearance - Cookies**.
 
-Furthermore you can display this list to your visitors by placing the shortcode [cookies] into any post or page. 
+You can also display this list to your visitors by placing the shortcode [cookies] into any post or page.
 
-WordPress uses several cookies under the hood by default. If you'd like to filter these out, you can use the shortcode [cookies-nowp]. If you like, you can replace the default separator between the cookie name and its value by adding your own string between the opening and closing shortcode tags, for example [cookies] ==> [\cookies]. 
+WordPress uses several cookies under the hood by default. If you'd like to filter these out, use the shortcode [cookies-nowp], which will show a message if no non-WordPress cookies are found.
 
-This plugin is based on code by David Artiss, who kindly made it available on GitHub. I've wrapped it up into this standalone plugin. 
+This plugin is based on code by David Artiss, who kindly made it available on GitHub.
 
 To find out more about what cookies are and what can be done with them, please read https://en.wikipedia.org/wiki/HTTP_cookie
 
 
 == Installation ==
 
-1. Either: Upload the entire folder `cookies` to the `/wp-content/plugins/` directory. 
-1. Or: download the ZIP file, then head over to Plugins - Add New - Install, then browse to your file
-1. Or: from Plugins - Add New, search for "show me the cookies", to find this plugin and hit "install"
-1. Then: Activate the plugin through the 'Plugins' menu in WordPress
-1. You can your cookies under Appearance - Cookies
+1. Either: Upload the entire folder `cookies` to the `/wp-content/plugins/` directory.
+1. Or: Download the ZIP file, then head over to Plugins - Add New - Upload Plugin, then browse to your file.
+1. Or: From Plugins - Add New, search for "show me the cookies" and hit Install.
+1. Then: Activate the plugin through the Plugins menu in WordPress.
+1. You can view your cookies under Appearance - Cookies.
 
 
 == Frequently Asked Questions ==
@@ -46,6 +46,20 @@ None so far.
 
 == Changelog ==
 
+= 1.1 =
+* Security: escaped all cookie name and value output to prevent XSS
+* Security: removed do_shortcode() wrapping on cookie data to prevent shortcode injection
+* Security: added direct file access guard
+* Security: removed third-party social media scripts from admin page
+* Fixed: undefined variables in admin cookie list function (PHP 8 compatibility)
+* Fixed: capability check mismatch between menu registration and access guard
+* Added: Text Domain declaration for translations
+* Updated: requires PHP 8.0 and WordPress 5.9 or later
+* Code: refactored into a class to avoid global namespace pollution
+* Improved: shortcode and admin cookie list now share a consistent format (Cookie #N / Value)
+* Improved: [cookies-nowp] shows a message when no non-WordPress cookies are found
+* Removed: custom separator feature (enclosing shortcode form [cookies]...[/cookies])
+
 = 1.0 =
 * Initial Release
-* based on code by David Artiss (thanks, David!)
+* Based on code by David Artiss (thanks, David!)
